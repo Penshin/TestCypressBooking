ab50f3fce434dcfe2c0466c1a9c09dc59c11d7e4
Stan P <penshin@gmail.com>
2023-01-03 10:13:03 -0800

    modify login test

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -3,19 +3,19 @@
 describe('Verify Register account and login', () => {
 
     it('Verify text link "Register account now', () => {
-        // cy.visit("/");
-        // cy.get('div.inner a.login').click();
-        // cy.get('#byemail div.col-sm-6 > input[data-check="email"]').type(Cypress.env('login'), {force: true});
-        // cy.get('#byemail input[name="password"]').type(Cypress.env('password'), {force: true});
-        // cy.get('#byemail input[value="SIGN IN"]').click();
+        cy.visit("/");
+        cy.get('div.inner a.login').click();
+        cy.get('#byemail div.col-sm-6 > input[data-check="email"]').type(Cypress.env('login'), {force: true});
+        cy.get('#byemail input[name="password"]').type(Cypress.env('password'), {force: true});
+        cy.get('#byemail input[value="SIGN IN"]').click();
         
         // cy.get('div > a:nth-child(5)')
         // .should('be.visible')
         // .should('have.text', 'Register account now');
         // cy.wrap(Cypress.env('login'))
         //     .should('have.key', 'manager')
-        cy.log('**login:** ' + Cypress.env().login)
-        cy.log('**password:** ' + Cypress.env().password)
+        // cy.log('**login:** ' + Cypress.env().login)
+        // cy.log('**password:** ' + Cypress.env().password)
 
         // save a screenshot of the test runner
         // to show what the env values were
